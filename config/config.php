d8011a1cffd414322f03a377fac7d1181bcdaa71
Landry2002 <pabdoulandry@gmail.com>
2026-03-28 23:58:28 +0000

    Premier envoi du projet Nextcloud

---
+++ config/config.php
@@ -0,0 +1,24 @@
+<?php
+$CONFIG = array (
+  'passwordsalt' => '2tITBNt0Sq4gkBH/lsIz1uZB/P4BDY',
+  'secret' => 'b3cJyMk5fjd/BuQ7XYj8ZEoDCbvw1MTb5GsS7s26YhWcrC/5',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+    1 => '192.168.11.110',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '32.0.7.1',
+  'overwrite.cli.url' => 'http://localhost',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'db',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'nextcloud',
+  'instanceid' => 'oc6d92c40vqm',
+  'installed' => true,
+  'overwriteprotocol' => 'https',
+  'trusted_proxies' => ['127.0.0.1'],
+);
